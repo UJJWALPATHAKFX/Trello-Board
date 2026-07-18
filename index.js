8b868acc8f4649d560f8f08499e37748d20911ae
Ujjwal <ujjwal2004p@gmail.com>
2026-07-18 11:09:17 +0530

    init

---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const users = [{
+    id:1,
+    username:"Ujjwal",
+    password:"12345"
+},{
+    id:2,
+    username:"raman",
+    password:"123456"
+}];
+const organizations = [{
+    id:1,
+    title:"100xdevs",
+    description: "Learning backend",
+    admin: 1,
+    members:[2]
+},{
+    id:2,
+    title:"raman org",
+    description: "Learning backend full ",
+    admin: 2,
+    members:[2]
+}];
+const boards = [{
+    id:1,
+    title:"100xschool website frontend",
+    organizationId:1
+}];
+const issues = [{
+    id:1,
+    title:"Add dark mode",
+    boardId: 1
+},
+{
+    id:2,
+    title:"Add like pop up feature",
+    boardId: 1 
+}];
+const app = express();
+app.listen(3000);
